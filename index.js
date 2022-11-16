851c7b3fa770e6817bfd5fa4cbf5e716e8f83247
Hiago <hiagolaureano57@gmail.com>
2022-11-16 12:01:19 -0300

    criação da api de cadastro de cliente

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const cors = require('cors')
 
+const fs = require('fs');
+const path = require('path');
+const formidable = require('formidable');
+
 const mysql = require('mysql2');
 const { ifError } = require('assert');
 
@@ -45,6 +49,20 @@ app.get("/api/usuario/listar", (req,res)=>{
         }
         res.status(200).send({output:result})
     });
+});
+
+app.get("/api/usuario/lista/:id", (req,res)=>{
+  connection.query(
+    "Select * From Usuario where idUsuario=? ",
+    [req.params.id],
+    (erro, result) =>{
+      if(erro){
+          return res
+          .status(400)
+          .send({output:`Erro ao tentar carregar dados ->${erro}`});
+      }
+      res.status(200).send({output:result})
+  });
 });
 
 app.post("/api/usuario/cadastro", (req, res) => {
@@ -165,6 +183,8 @@ app.post("/api/usuario/cadastro", (req, res) => {
     });
 });
 
+
+
 app.delete("/api/pet/apagar/:id", (req, res) => {
   connection.query(
     "Delete from Pet where idPet=?",
@@ -216,7 +236,17 @@ app.post("/api/larPet/cadastro", async(req,res)=>{
         .status(400)
         .send({ output: `Erro ao tentar cadastrar -> ${erro}` });
     }
+
+
+   
+
+
+
+
+
+
     res.status(201).send({ output: `Cadastro realizado`, payload: result });
+    
   });
 });
 
@@ -255,14 +285,28 @@ app.get("/api/cliente/listar/:id", (req,res)=>{
   })
 })
 
-app.post("/api/publihall/cadastro", async(req,res)=>{
-  connection.query("INSERT INTO HallPet SET ?", [req.body], (erro, result) => {
+app.post("/api/publihall/cadastro", (req,res,next)=>{
+   
+ connection.query("INSERT INTO HallPet SET ?", [req.body], (erro, result) => {
     if (erro) {
       return res
         .status(400)
         .send({ output: `Erro ao tentar cadastrar -> ${erro}` });
     }
-    res.status(201).send({ output: `Cadastro realizado`, payload: result });
+   
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      console.log(files);
+      const oldpath = files.foto.filepath;
+      console.log(`nome do arquivo ${oldpath}`);
+      console.log(files);
+          const newpath = path.join(__dirname, './upload', files.foto.originalFilename);
+          
+          fs.renameSync(oldpath, newpath);
+         return res.status(201).send({ output: `Cadastro realizado`, payload: result });
+         next();
+    });
+ 
   });
 });
 
@@ -283,6 +327,7 @@ app.get("/", async (req,res)=>{
 
 function verificar(req, res, next) {
   const token_enviado = req.headers.token;
+  console.log(req.headers.token)
 
   if (!token_enviado) {
     return res.status(401).send({
@@ -306,6 +351,207 @@ function criarToken(id, usuario, email) {
       expiresIn: "2d",
     });
   }
+
+
+  app.post('/subirfotos', (req, res, next) => {
+    console.log(`Dados ->${req}`)
+    const formidable = require('formidable');
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      console.log(files);
+      const oldpath = files.foto.filepath;
+      console.log(`nome do arquivo ${oldpath}`);
+      console.log(files);
+          const newpath = path.join(__dirname, './upload', files.foto.originalFilename);
+          
+          fs.renameSync(oldpath, newpath);
+          //res.status(200).send('File uploaded and moved!');
+          next();
+    });
+
+    connection.query("INSERT INTO HallPet SET ?", [req.body], (erro, result) => {
+      if (erro) {
+        return res
+          .status(400)
+          .send({ output: `Erro ao tentar cadastrar -> ${erro}` });
+      }
+      res.status(201).send({output:result})
+    });
+  });
+
+  app.get("/api/editar/listar/:id", (req,res)=>{
+    connection.query(`select cl.idCliente, cl.nomeCliente, cl.foto , 
+    en.idEndereco,
+    en.tipo,
+    en.logradouro,
+    en.numero,
+    en.complemento,
+    en.cep,
+    en.bairro,
+    en.cidade,
+    en.estado,
+    dp.idDadosPessoais,
+    dp.cpf,
+    dp.sexo,
+    dp.dataNascimento,
+    ct.idContato,
+    ct.email, 
+    ct.telefoneCelular,
+    ct.telefoneResidencial,
+    us.idUsuario,
+    us.nomeUsuario
+    From Cliente cl inner join Endereco en on cl.idEndereco = en.idEndereco  
+    inner join DadosPessoais dp on cl.idDadosPessoais =  dp.idDadosPessoais
+    inner join Contato ct on cl.idContato = ct.idContato 
+    inner join Usuario us on cl.idUsuario = us.idUsuario
+    where cl.idUsuario = ?`, [req.params.id], (erro, result)=>{
+      if(erro){
+        return res
+        .status(400)
+        .send({output:`Erro ao tentar carregar dados ->${erro}`});
+    }
+    res.status(200).send({output:result})
+    })
+  })
+
+  let DP = 0;
+  let EN = 0;
+  let CT = 0;
+
+  app.post("/api/perfil/cadastro", (req, res) => {
+
+
+    connection.query(
+      "INSERT INTO  DadosPessoais set cpf = ?, dataNascimento = ?, sexo = ? ",
+      [req.body.cpf,req.body.dataNascimento,req.body.sexo, req.body.idDadosPessoais],
+      async(erro, resultdp) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        connection.query(
+          "INSERT INTO  Endereco set tipo = ?, logradouro = ?, numero = ?, complemento = ?, cep = ?, bairro = ?, cidade = ?, estado = ? ",
+          [req.body.tipo,req.body.logradouro,req.body.numero,req.body.complemento,req.body.cep,req.body.bairro,req.body.cidade,req.body.estado, req.body.idEndereco],
+          async (erro, result) => {
+            if (erro) {
+              return res
+                .status(400)
+                .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+            }
+            EN = await result.insertId;
+            //res.status(200).send({ output: `Dados atualizados`, payload: result });
+          }
+        )
+    
+      }
+    )
+
+    connection.query(
+      "INSERT INTO  Endereco set tipo = ?, logradouro = ?, numero = ?, complemento = ?, cep = ?, bairro = ?, cidade = ?, estado = ? ",
+      [req.body.tipo,req.body.logradouro,req.body.numero,req.body.complemento,req.body.cep,req.body.bairro,req.body.cidade,req.body.estado, req.body.idEndereco],
+      async (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        EN = await result.insertId;
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+
+    connection.query(
+      "INSERT INTO  Contato set telefoneResidencial = ?, telefoneCelular = ?, email = ?",
+      [req.body.telefoneResidencial,req.body.telefoneCelular,req.body.email],
+      async (erro, result) => {
+        if (erro) {
+          console.log( `Erro ao tentar atualizar -> ${erro}`);
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        CT = await result.insertId;
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+    console.log(`Dados da tabela -> ${DP},${EN},${CT}`)
+    
+    connection.query(
+      "INSERT INTO Cliente set nomeCliente = ?, idDadosPessoais = ?, idEndereco = ?, idContato = ?, idUsuario = ? ",
+      [req.body.nomeCliente,DP,EN,CT,req.body.idUsuario],
+      (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+     res.status(200).send({ output: `Dados Cadastrados com Sucesso` });
+  });
+
+  app.put("/api/perfil/atualizar/:id", verificar, (req, res) => {
+
+    console.log(`dados -> ${req.body.nomeUsuario} - ${req.body.nomeCliente} - ${req.body.email}`)
+
+    connection.query(
+      "Update Usuario set nomeUsuario = ? where idUsuario=?",
+      [req.body.nomeUsuario, req.params.id],
+      (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .json({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    );
+
+    connection.query(
+      "Update DadosPessoais set cpf = ?, dataNascimento = ?, sexo = ? where idDadosPessoais=?",
+      [req.body.cpf,req.body.dataNascimento,req.body.sexo, req.body.idDadosPessoais],
+      (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+
+    connection.query(
+      "Update Endereco set tipo = ?, logradouro = ?, numero = ?, complemento = ?, cep = ?, bairro = ?, cidade = ?, estado = ? where idEndereco=?",
+      [req.body.tipo,req.body.logradouro,req.body.numero,req.body.complemento,req.body.cep,req.body.bairro,req.body.cidade,req.body.estado, req.body.idEndereco],
+      (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+
+    connection.query(
+      "Update Contato set telefoneCelular = ?, email = ? where idContato=?",
+      [req.body.telefoneCelular,req.body.email, req.body.idContato],
+      (erro, result) => {
+        if (erro) {
+          return res
+            .status(400)
+            .send({ output: `Erro ao tentar atualizar -> ${erro}` });
+        }
+        //res.status(200).send({ output: `Dados atualizados`, payload: result });
+      }
+    )
+     res.status(200).send({ output: `Dados atualizados` });
+  });
+  
+
+
 
 
 app.listen(3000);
